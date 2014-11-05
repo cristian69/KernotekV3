393d74d4fcc2d3f02335ee6eb4fc0a375c81f7d6
Cristian <cr7ramirez69@gmail.com>
2014-11-05 16:52:45 -0600

    Agregando el menu y dando nuevos estilos al modulo principal

--- static/scripts/jquery_kernotek.js
+++ static/scripts/jquery_kernotek.js
@@ -36,45 +36,6 @@ function redireccionar(valor, direccion){
 }
 
 
-function refrescar(valor){
-	this.valor=valor;
-	if(valor=="si"){
-		setTimeout(function(){
-			window.location.reload(true);
-		}, 20000);	
-	}
-	if(valor=="no"){
-		setTimeout(function(){
-			window.location.reload(true);
-		},1000000);
-	}
-	valor=""
-;}
-
-
-function Mensajes(valor){
-	if(valor=="ActivarCuentas"){
-		$("#MensajeActivar").removeClass("hidden");
-		$("#MensajeActivar" ).animate({
-			opacity: 0.0,
-		}, 3500 );
-
-		setTimeout(function () {
-			$('#MensajeActivar').alert('close');
-		}, 3500);
-	}
-
-	if(valor=="BorrarCuentas"){
-		$("#MensajeBorrar").removeClass("hidden");
-		$("#MensajeBorrar" ).animate({
-			opacity: 0.0,
-		}, 3500 );
-
-		setTimeout(function () {
-			$('#MensajeBorrar').alert('close');
-		}, 3500);
-	}
-}
 
 
 $("#configCorte").mouseover(function(){
@@ -96,82 +57,6 @@ $("#configCorte").mouseover(function(){
 });
 
 
-$("#Monedero").mouseover(function(){
-	if($("#activoMoneda10").prop("checked")==true){
-		$("#m10").val("1");	
-	}
-	else{
-		$("#m10").val("0");	
-	}
-	if($("#activoMoneda5").prop("checked")==true){
-		$("#m5").val("1");	
-	}
-	else{
-		$("#m5").val("0");	
-	}
-	if($("#activoMoneda2").prop("checked")==true){
-		$("#m2").val("1");	
-	}
-	else{
-		$("#m2").val("0");	
-	}
-	if($("#activoMoneda1").prop("checked")==true){
-		$("#m1").val("1");	
-	}
-	else{
-		$("#m1").val("0");	
-	}
-	if($("#activoMoneda05").prop("checked")==true){
-		$("#m05").val("1");	
-	}
-	else{
-		$("#m05").val("0");	
-	}
-});
-
-
-$("#Billetero").mouseover(function(){
-	if($("#activoBillete500").prop("checked")==true){
-		$("#b500").val("1");	
-	}
-	else{
-		$("#b500").val("0");	
-	}
-	if($("#activoBillete200").prop("checked")==true){
-		$("#b200").val("1");	
-	}
-	else{
-		$("#b200").val("0");	
-	}
-	if($("#activoBillete100").prop("checked")==true){
-		$("#b100").val("1");	
-	}
-	else{
-		$("#b100").val("0");	
-	}
-	if($("#activoBillete50").prop("checked")==true){
-		$("#b50").val("1");	
-	}
-	else{
-		$("#b50").val("0");	
-	}
-	if($("#activoBillete20").prop("checked")==true){
-		$("#b20").val("1");	
-	}
-	else{
-		$("#b20").val("0");	
-	}
-});
-
-
-$("#ConfiguracionReporte").mouseover(function(){
-	$("#valorticket").val($("#tic").val());
-	$("#valorturno").val($("#tur").val());
-	$("#valorfecha").val($("#fec").val());
-	$("#valortarifa").val($("#tar").val());
-	$("#valortotal").val($("#tot").val());
-	$("#valorCambio").val($("#cam").val());
-});
 
 
 $("#ReportesAceptar").click(function(){
@@ -183,17 +68,6 @@ $("#ReportesAceptar").click(function(){
 });
 
 
-function EReporte(estado){
-	if(estado=="si"){
-		$("#tur").select2();
-		$("#fec").select2();
-		$("#tar").select2();
-		$("#tic").select2();
-		$("#tot").select2();
-		$("#cam").select2();
-	}
-}
-
 
 function reporteTurno(sa){
 	$("#valorTurno").select2();
@@ -201,20 +75,6 @@ function reporteTurno(sa){
 }
 
 
-function CSistema(Nvalor){
-	if(Nvalor=="si"){
-		$("#moneda10").select2();
-		$("#moneda5").select2();
-		$("#moneda2").select2();
-		$("#moneda1").select2();
-		$("#moneda05").select2();
-		$("#billetero500").select2();
-		$("#billetero200").select2();
-		$("#billetero100").select2();
-		$("#billetero50").select2();
-		$("#billetero20").select2();	
-	}
-}
 
 function tipoReporte(){
 	$("#tipoReporte").select2({
@@ -232,69 +92,6 @@ function llavesSistema(){
 }
 
 
-$("#aceptarCambio").click(function(){
-	nContraseña=$("#nuevoPassword").val();
-	cContraseña=$("#confirmarPassword").val();
-	$('#passwordActual').focus();
-	ventanaAlerta=document.getElementById("textoAlerta");
-	if(nContraseña.length == 0 && cContraseña.length == 0){
-		ventanaAlerta.innerHTML="Algunos de los campos del formulario están vacíos, ingresa los valores correspondientes.";
-		$("article.mensajesPerfil").show();
-		$("#cambioContraseña2").removeClass("has-success");
-	    $("#cambioContraseña3").removeClass("has-success");
-	    $("#cambioContraseña2").addClass("has-error");
-        $("#cambioContraseña3").addClass("has-error");
-        return false;
-	}
-
-	if(nContraseña.length<6){
-		ventanaAlerta.innerHTML="Las contraseñas deben de contener por lo menos 6 caracteres.";
-		$("article.mensajesPerfil").show();
-		$("#nuevoPassword").val("");
-		$("#confirmarPassword").val("");
-		$("#cambioContraseña2").removeClass("has-success");
-	    $("#cambioContraseña3").removeClass("has-success");
-	    $("#cambioContraseña2").addClass("has-error");
-        $("#cambioContraseña3").addClass("has-error");
-        return false;
-	}
-
-	if(nContraseña != cContraseña){
-		ventanaAlerta.innerHTML="Las contraseñas no coinciden. ¿Quieres volver a intentarlo?.";
-		$("article.mensajesPerfil").show();
-		$("#confirmarPassword").val("");
-		$("#nuevoPassword").val("");
-		$("#cambioContraseña2").removeClass("has-success");
-	    $("#cambioContraseña3").removeClass("has-success");
-	    $("#cambioContraseña2").addClass("has-error");
-        $("#cambioContraseña3").addClass("has-error");
-		return false;
-		}
-
-	if(/\s/.test(nContraseña)){
-	    ventanaAlerta.innerHTML="Las Contraseñas No pueden Contener Espacios en Blanco";
-		$("article.mensajesPerfil").show();
-		$("#nuevoPassword").val("");
-		$("#confirmarPassword").val("");
-		$("#cambioContraseña2").removeClass("has-success");
-	    $("#cambioContraseña3").removeClass("has-success");
-	    $("#cambioContraseña2").addClass("has-error");
-        $("#cambioContraseña3").addClass("has-error");
-		return false;
-	}
-});
-
-$("#aceptarCodigo").click(function(){
-	var codigo=$("#codigoLlave").val();
-	var ventana=document.getElementById("textoAlerta");
-	if(!/^[0-9A-Fa-f]{12}?$/.test(codigo)){
-		ventana.innerHTML="El código de la llave es incorrecta, ingrese un código válido.";
-		$("article.mensajesLlaves").show();
-		$("#codigoLlave").val("");
-		return false;
-	}
-});
-	
 
 
 function mostrarVentanas(parametro){
@@ -417,7 +214,7 @@ function graficas(){
 					pointStrokeColor : "#fff",
 					pointHighlightFill : "#fff",
 					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [90,30,10,80,15,5,15]
+					data : [90,30,10,80]
 				}
 			]
 
