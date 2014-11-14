ef219526a64516d0feaec45daed15f90dcbddad2
Cristian <cr7ramirez69@gmail.com>
2014-11-14 17:17:15 -0600

    Agregando funcion para validar tarifa

--- static/scripts/core/lqueryKernotekV3.js
+++ static/scripts/core/lqueryKernotekV3.js
@@ -195,21 +195,18 @@ $("#anteriorCorte").click(function(){
 
 
 function validarNumeros(){
-	numeroValidar=$("#nTarifa").val();
-	if(!/^[0-9]{1,5}(\.[0-9]{0,2})?$/.test(numeroValidar) || !/^[0-9]{1,3}$/.test(numeroValidar)){
-		$("#errorFormato").removeClass("display-none");
-		$("#"+valorId).val()="";
-	}
-	else{
-		$("#errorFormato").addClass("display-none");
-		$("#paso2").addClass("active");
-		$("#paso1").removeClass("active");
-		$("#tab2").addClass("active");
-		$("#tab1").removeClass("active");
-	document.getElementById("btnTarifa").innerHTML="<li><button type='button'class='btn' onclick='regresarTarifa();'>Regresar</button><button type='submit' class='btn blue' name='submit'>Aceptar</button></li>"
+	numeroValidar=$("#nuevaTarifa").val();
+	var patron =/^[0-9]+$/
+	var patron2=/\s/;
+	if(patron2.test(numeroValidar) || $("#nuevaTarifa").val()=="" || !patron.test(numeroValidar)){
+		$("#tarifaIncorrecta").removeClass("hidden");
+		cancelar();
+		return false;
 	}
 }
-
+function cancelar(){
+	return false;
+}
 $("#modalTurno").mouseover(function(){
 	if($("#tiposCortes").val()=="manual"){
 		$("#ConfirmarAccion").removeClass("hidden");
