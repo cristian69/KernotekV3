70af394b5e905679b61d2b6141a9083b324e5534
Jesus Adrian Ramirez Lopez <jadrianramirezl@gmail.com>
2014-11-25 16:14:58 -0600

    agregango variables a la vista

--- configuracionSistema.py
+++ configuracionSistema.py
@@ -3,12 +3,13 @@ import flask
 from flask import render_template, redirect, session, request, url_for
 import class_db
 from estado_sistema import sistema
-
+import config_sistema
 class Configuracion(flask.views.MethodView):
 	def get(self):
 		if len(session) > 1:
 			stateSystem = sistema()
-			return render_template('configuracionSistema.html', estado_sistema = stateSystem)
+			botonSystem = config_sistema.estadoSistema()
+			return render_template('configuracionSistema.html', estado_sistema = stateSystem, botonSistema=botonSystem)
 
 		else:
 			return redirect(url_for('login'))
