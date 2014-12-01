207549911e01b6e2705b756202b1d269ad464007
root <root@localhost.localdomain>
2014-12-01 08:52:03 -0600

    Agregando estilo a los modulos principales

--- static/plugins/chart/Chart.js
+++ static/plugins/chart/Chart.js
@@ -2835,7 +2835,7 @@
 		animateScale : false,
 
 		//String - A legend template
-		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
+		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend+1000\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
 	};
 
 
