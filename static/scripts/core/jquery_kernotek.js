f0eb4d87c69615ac98b3e607c73bf13067c54e5e
root <root@localhost.localdomain>
2014-12-17 10:02:23 -0600

    Agregando la validacion para no aceptar valores mayores a 650

--- static/scripts/core/jquery_kernotek.js
+++ static/scripts/core/jquery_kernotek.js
@@ -173,14 +173,15 @@ $(document).keypress(function(e) {
 function validateRate(){
 	numeroValidar=$("#nuevaTarifa").val();
 	var patron1=/\s/;
-	var patron2=/^([0-6]{0,1})?([0-4]{0,1})?([0-9]{0,1})?([\.]?([0,5]{0,1}))?([0]{0,1})?$/;
+	var patron2=/^([0-9]{1,3})?([0-9]{1,3})?([\.]?([0,5]{0,1}))?([0]{0,1})?$/;
+	var limiteTarifa=parseFloat("650.50");
 	if(numeroValidar==="."){
 		$("#tarifaIncorrecta").removeClass("hidden");
 		$("#nuevaTarifa").val("");
 		$('#nuevaTarifa').focus();
 		return false;
 	}
-	if(!patron2.test(numeroValidar) || $("#nuevaTarifa").val()=="" || patron1.test(numeroValidar) || numeroValidar >= 651){
+	if(!patron2.test(numeroValidar) || $("#nuevaTarifa").val()=="" || patron1.test(numeroValidar) || numeroValidar >=limiteTarifa){
 		$("#tarifaIncorrecta").removeClass("hidden");
 		$("#nuevaTarifa").val("");
 		$('#nuevaTarifa').focus();
